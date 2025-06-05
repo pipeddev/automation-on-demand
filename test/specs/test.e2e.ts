ce6095a909345c613e51ba79e5518068db160bdc
Felipe Carrasco <carrascogalindo.felipe@gmail.com>
2025-06-05 11:54:46 -0400

    feat: improve test

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -14,16 +14,3 @@ describe("My Login application", () => {
     await expect(SecurePage.flashAlert).toMatchSnapshot("flashAlert");
   });
 });
-
-describe("My Login application Error", () => {
-  it("should login with valid credentials fail", async () => {
-    await LoginPage.open();
-
-    await LoginPage.login("tomsmith", "SuperSecretPasswor!");
-    await expect(SecurePage.flashAlert).toBeExisting();
-    await expect(SecurePage.flashAlert).toHaveText(
-      expect.stringContaining("You logged into a secure area!")
-    );
-    await expect(SecurePage.flashAlert).toBeDisplayed();
-  });
-});
